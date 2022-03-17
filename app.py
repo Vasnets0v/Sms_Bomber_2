8acb5b8a6f87e94e263252da006605e567a68691
Vasnets0v <byvasnetsov@gmail.com>
2022-03-17 20:53:48 +0200

    can handle single field pages

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import random
 import pyfiglet
 import os
 
+print(pyfiglet.figlet_format("SMS BOMBER 2"))
+
 
 def collect_info():
     init_info = {'phone_num': int(input('Enter phone number:')),
@@ -18,6 +20,7 @@ def collect_info():
 
 def collect_sites_with_elements():
     sites = []
+
     for file in os.listdir('dictionary'):
         if file.endswith('.txt'):
             sites.append(file)
@@ -28,28 +31,40 @@ def collect_sites_with_elements():
         with open(f'dictionary/{site}') as temp_file:
             elements_list = []
             for element in temp_file:
-                elements_list.append(element)
+                elements_list.append(element.split('\n')[0])
             sites_with_elements.append({'site': site, "items": elements_list})
     return sites_with_elements
 
 
-if __name__ == "__main__":
-    print(collect_sites_with_elements())
+def process_one_field_page(phone_number):
+    sites_with_elements = collect_sites_with_elements()
 
-#            'webdrivers' folder should contain the same chrome driver version as your chrome browser version
-#             service = Service(executable_path="webdrivers/chromedriver.exe")
-#             browser = webdriver.Chrome(service=service)
-#
-#             browser.get(item["url"])
-#             time.sleep(random.randint(2, 5))
-#             field_on_page = browser.find_element(By.XPATH, value=item['xpath'])
-#             # sometime need to focus on element .click()
-#             field_on_page.click()
-#             field_on_page.clear()
-#             field_on_page.send_keys(phone_number)
-#             time.sleep(1)
-#             field_on_page.send_keys(Keys.ENTER)
-#             time.sleep(random.randint(2, 5))
-#             input()
-#             browser.close()
-#             browser.quit()
+    for site in range(len(sites_with_elements)):
+        service = Service(executable_path="webdrivers/chromedriver.exe")
+        browser = webdriver.Chrome(service=service)
+
+        current_dict_from_list = sites_with_elements[site]
+        num_items_on_site = len(current_dict_from_list["items"])
+
+        browser.get(f'https://{current_dict_from_list["items"][0]}/')
+        time.sleep(random.randint(2, 5))
+
+        for i in range(num_items_on_site):
+            if i > 0:
+                if i <= num_items_on_site - 1:
+                    field_on_page = browser.find_element(By.XPATH, value=current_dict_from_list["items"][i])
+                    field_on_page.click()
+                    time.sleep(random.randint(2, 5))
+
+        field_on_page = browser.find_element(By.XPATH, value=current_dict_from_list["items"][num_items_on_site - 1])
+        field_on_page.click()
+        field_on_page.send_keys(phone_number)
+        time.sleep(random.randint(2, 5))
+        field_on_page.send_keys(Keys.ENTER)
+        time.sleep(random.randint(2, 5))
+        browser.close()
+        browser.quit()
+
+
+if __name__ == "__main__":
+    process_one_field_page("0")
